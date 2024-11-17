717e469e43d9d194ec8d8936daf12b0f10062b80
Anthony <brayan.farronan@gmail.com>
2024-11-17 16:10:20 +0100

    commit 06: Method post user

--- src/routes/UsersRoutes.py
+++ src/routes/UsersRoutes.py
@@ -1,11 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from src.services.UsersServices import UsersService
+from src.models.PersonModel import Person
 
 main = Blueprint('users_blueprint',__name__)
 
-@main.route('/')
+@main.route('/',methods=['GET'])
 def get_users():
     UsersService.get_user()
     print('Esto se imprime en consola')
+    return 'Esto en la página'
+
+@main.route('/',methods=['POST'])
+def post_users():
+    print(request)
+    dni = request.json['dni']
+    name_person= request.json['name_person']
+    phone = request.json['phone']
+
+    user= (Person(dni, name_person, phone))
+    post_person= UsersService.post_user(user)
+    print(post_person)
+
+    # print(put_person)
     return 'Esto en la página'
--- src/services/UsersServices.py
+++ src/services/UsersServices.py
@@ -1,4 +1,8 @@
-from src.database.db_mysql import get_connection
+from src.database.db_mysql import get_connection;
+
+#Model
+from src.models.PersonModel import Person
+
 
 class UsersService():
 
@@ -15,6 +19,26 @@ class UsersService():
 
                 connection.close()
                 return 0
+            
+        except Exception as ex:
+            print(ex)
+    
+    @classmethod
+    def post_user(cls, person:Person):
+        try:
+            connection= get_connection()
+            print(connection)
+
+            with connection.cursor() as cursor:
+                dni = person.dni
+                name_person= person.name_person
+                phone = person.phone
+
+                cursor.execute("insert into person(dni, name_person, phone) values ('{0}', '{1}', '{2}');".format(dni, name_person, phone))
+                connection.commit()
+
+                connection.close()
+                return 'Este es el método post, se imprime en consola'
             
         except Exception as ex:
             print(ex)
