6c87d4e0981297295335a3e94d176a857a0d28d2
Anthony <brayan.farronan@gmail.com>
2024-11-17 15:00:27 +0100

    Commit 04: Method get person

--- src/services/UsersServices.py
+++ src/services/UsersServices.py
@@ -5,8 +5,16 @@ class UsersService():
     @classmethod
     def get_user(cls):
         try:
-            connection= get_connection
+            connection= get_connection()
             print(connection)
+
+            with connection.cursor() as cursor:
+                cursor.execute('Select * from person')
+                result= cursor.fetchall()
+                print(result)
+
+                connection.close()
+                return 0
             
         except Exception as ex:
             print(ex)
