5f13e32f7213559dcb335cf3fd3ceb0d6e89a386
Anthony <brayan.farronan@gmail.com>
2024-11-24 13:06:52 +0100

    Commit 15: Verification of fields

--- src/routes/UsersRoutes.py
+++ src/routes/UsersRoutes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify,request
 
 from src.services.UsersServices import UsersService
 from src.models.UsersModel import Users
@@ -7,24 +7,55 @@ main = Blueprint('users_blueprint',__name__)
 
 @main.route('/',methods=['GET'])
 def get_users():
-    UsersService.get_user()
-    print('Esto se imprime en consola')
-    return 'Esto en la página'
+
+    try:
+        UsersService.get_user()
+        print('Esto se imprime en consola')
+        return 'Esto en la página'
+    
+    except Exception as ex:
+     print(ex)
 
 @main.route('/',methods=['POST'])
 def post_users():
-    print(request)
-    name_usuario= request.json['name_usuario']
-    password = request.json['password']
-    fk_id_type_user = request.json['fk_id_type_user']
-    fk_dni = request.json['fk_dni']
+    try:
+        print(request)
 
-    user= (Users(None,name_usuario, password, fk_id_type_user, fk_dni))
-    post_user= UsersService.post_user(user)
-    print(post_user)
+        required_fields = ['name_usuario', 'password', 'fk_id_type_user', 'fk_dni']
+        errors = []
 
-    return 'Esto en la página'
+        for field in required_fields:
+            if field not in request.json:
+                errors.append(f"{field} es obligatorio")
 
+        # Si hay errores, devolver una respuesta con los mismos
+        if errors:
+            return jsonify({"errors": errors}), 400
+
+        name_usuario= request.json['name_usuario']
+        password = request.json['password']
+        fk_id_type_user = request.json['fk_id_type_user']
+        fk_dni = request.json['fk_dni']
+
+        if ('name_usuario' not in request.json):
+            errors.append("El campo 'username' es obligatorio")
+        elif('password' not in request.json):
+            errors.append("El campo 'password' es obligatorio")
+        elif('fk_id_type_user' not in request.json):
+            errors.append("El campo 'Tipo de usuario' es obligatorio")
+        elif('fk_dni' not in request.json):
+            errors.append("El campo 'DNI' es obligatorio")
+
+        user= (Users(None,name_usuario, password, fk_id_type_user, fk_dni))
+        post_user= UsersService.post_user(user)
+        print(post_user)
+
+        return 'Esto en la página'
+    except Exception as ex:
+     print(ex)
+     response = jsonify({'message': 'Internal Server Error'})
+     return response, 500
+    
 @main.route('/',methods=['PUT'])
 def put_users():
 
